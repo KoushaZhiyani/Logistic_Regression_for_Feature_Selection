1777fa25f4389531fc4d70044ce878bace420868
Kousha <150776406+KoushaZhiyani@users.noreply.github.com>
2024-01-02 13:43:09 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-
 while True:
-    status = input("1)start\n0)Exit!")
+    status = input("1)start\n0)Exit!\n")
     if status == "0":
         exit()
     # Address your csv file
@@ -17,10 +16,11 @@ while True:
     # select Target columns
     target = input("target : \n")
     # A maximum of columns ( Feature )should be checked
-    columns_num = int(input("number of columns"))
+    columns_num = int(input("number of columns : \n"))
     data = pd.read_csv(address)
     # find best Features
     corr = data.corr()[target].sort_values(ascending=False)
+
     corr = list(corr.index)
     corr.remove(target)
     # Variable for columns ( Feature )
@@ -33,36 +33,24 @@ while True:
     for i in range(1, columns_num + 1):
         score = []
         data = pd.read_csv(address)
-        X = data[columns]
+        X = data[columns[:i + 1]]
         y = data[target]
-        model = LogisticRegression()
-        rfe = RFE(model, n_features_to_select=i)
-        rfe.fit(X, y)
-        # Variable, show  which columns selected
-        sets = rfe.support_
-        all_sets.append(sets)
-        print(sets)
-        for j in range(0, columns_num):
-            if sets[j] == False:
-                X = X.drop(columns[j], axis=1)
         # selecting different samples group
         for j in range(0, 100):
-            model = LogisticRegression(max_iter=160)
-            X_train, X_test, y_train, y_tset = train_test_split(X, y, test_size=0.2, random_state=j)
+            model = LogisticRegression(max_iter=2000)
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=j)
             model.fit(X_train, y_train)
+            # print(model.feature_names_in_)
             y_pred = model.predict(X_test)
-            score.append(model.score(X_test, y_tset))
-
+            score.append(model.score(X_test, y_test))
         avg_score.append(sum(score) / 100)
+
     # find Features from max score
     highest_scored = max(avg_score)
     highest_scored_index = avg_score.index(highest_scored)
-    c = 0
+    print(print(round(np.mean(max(avg_score)), 3), round(np.std(max(avg_score)),3)))
     print("Features :")
-    for j in all_sets[highest_scored_index]:
-        if j:
-            print(columns[c])
-        c = c + 1
+    print(columns[:highest_scored_index + 1])
     # Data visualization
     plt.scatter(range(1, columns_num + 1), avg_score, c="green")
     plt.xlabel("number of columns")
